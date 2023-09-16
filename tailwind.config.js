b9a639e07b34e4beb07a44ac52834ccc5d2d8bc6
Tomiwa Bada <tomiwabd@gmail.com>
2023-09-16 22:37:38 +0100

    Background fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'hero-poster': "url('./src/assets/Poster.png')",
-      }
+        "hero-poster": "url('./assets/Poster.png')",
+      },
     },
   },
   plugins: [],
-}
-
+};
